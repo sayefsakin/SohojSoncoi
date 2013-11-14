cc6a6df439e77ed5dc1141f5f27ddfdc63cfbc68
sayefsakin <sayefsakin@yahoo.co.in>
2013-11-14 13:09:17 +0600

    home manu created

--- src/com/sakin/sohojshoncoi/Main.java
+++ src/com/sakin/sohojshoncoi/Main.java
@@ -1,28 +1,14 @@
 package com.sakin.sohojshoncoi;
 
-import com.j256.ormlite.android.apptools.OpenHelperManager;
-import com.j256.ormlite.dao.RuntimeExceptionDao;
-import com.sakin.sohojshoncoi.database.Account;
-import com.sakin.sohojshoncoi.database.Category;
-import com.sakin.sohojshoncoi.database.DatabaseHelper;
-import com.sakin.sohojshoncoi.database.Media;
-import com.sakin.sohojshoncoi.database.MediaCategory;
-import com.sakin.sohojshoncoi.database.Reminder;
 import com.sakin.sohojshoncoi.database.SSDAO;
-import com.sakin.sohojshoncoi.database.Transaction;
 import com.sakin.sohojshoncoi.daylihisab.DayliHisabMain;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.text.Html;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class Main extends Activity {
 
@@ -31,21 +17,7 @@ public class Main extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		init();
-
-		Account account = new Account("Sakin",0.0,0.0);
-		SSDAO.getSSdao().getAccountDAO().create(account);
-		
-		WebView webView = (WebView)findViewById(R.id.test1);
-		WebSettings settings = webView.getSettings();
-		settings.setDefaultTextEncodingName("utf-16");
-		String htmlString = "আমার বাংলা";
-//		webView.loadDataWithBaseURL(null, htmlString, "text/html", "utf-16", null);
-		webView.loadData(htmlString, "text/html", "utf-16");
-		
-		
-		TextView tv = (TextView)findViewById(R.id.textView1);
 		Button dayliHisabB = (Button)findViewById(R.id.dayliHisabB);
-//		dayliHisabB.setText(R.string.bangla);
 		dayliHisabB.setOnClickListener(new OnClickListener() {
 			
 			public void onClick(View arg0) {
